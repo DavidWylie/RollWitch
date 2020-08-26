58d479562b327058753a75af535114aa742e0c6f
DavidWylie <david.leo.wylie@googlemail.com>
2020-08-26 18:34:39 +0100

    Add basic threading

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import os
-
-import discord
-from dotenv import load_dotenv
-from roll_witch import roller
+from roll_witch import bot
+from threading import Thread
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
-
-
 @app.route('/')
 def root():
     return "Hello World"
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    for guild in client.guilds:
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
-        )
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!roll'):
-        response = roller.roll_percentile(message.content[6:], message.author.display_name)
-        await message.channel.send(response)
-    elif message.content.startswith('!r'):
-        response = roller.roll_percentile(message.content[3:], message.author.display_name)
-        await message.channel.send(response)
-
 
 if __name__ == '__main__':
-    client.run(TOKEN)
+    bot_thread = Thread(target=bot.start_bot, name='discord-bot-thread', daemon=True)
+    bot_thread.start()
     app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ roll_witch/bot.py
@@ -0,0 +1,35 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+from roll_witch import roller
+
+
+def start_bot():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+        for guild in client.guilds:
+            print(
+                f'{client.user} is connected to the following guild:\n'
+                f'{guild.name}(id: {guild.id})'
+            )
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('!roll'):
+            response = roller.roll_percentile(message.content[6:], message.author.display_name)
+            await message.channel.send(response)
+        elif message.content.startswith('!r'):
+            response = roller.roll_percentile(message.content[3:], message.author.display_name)
+            await message.channel.send(response)
+
+    client.run(TOKEN)
