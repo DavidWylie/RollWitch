da4444d495c9c79af8ff14155cdcbe3c8aa3e824
DavidWylie <david.leo.wylie@googlemail.com>
2020-08-26 17:41:32 +0100

    add app from flask to let this work

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ import discord
 from dotenv import load_dotenv
 from roll_witch import roller
 
+
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+@app.route('/')
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
 
 @client.event
 async def on_ready():
@@ -35,3 +54,4 @@ async def on_message(message):
 
 if __name__ == '__main__':
     client.run(TOKEN)
+    app.run(host='127.0.0.1', port=8080, debug=True)
