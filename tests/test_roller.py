c3635b768a7e7cf30c5a47a0ee04f7ec858480ae
DavidWylie <david.leo.wylie@googlemail.com>
2020-09-23 20:22:16 +0100

    seperate number generator into seperate class so its easier to replace.

--- tests/test_roller.py
+++ tests/test_roller.py
@@ -1,62 +1,72 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from roll_witch.dice_bot.roller import StandardRoller
+from roll_witch.dice_bot.roller import StandardRoller, generator
+from roll_witch.dice_bot.roller.generator import RandomNumberGenerator
 from roll_witch.dice_bot.spec import RollSpec
 from roll_witch.dice_bot.result import RollResult
 
 
+class TestGenerator(RandomNumberGenerator):
+    def __init__(self, numbers: list) -> None:
+        super().__init__()
+        self.numbers = numbers
+
+    def get_int(self, less_than):
+        return self.numbers.pop(0)
+
+
 class TestStandardRoller(TestCase):
-    @patch("random.randint")
-    def test_roll_dice_set_single_die_no_modifier(self, mock_random):
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_set_single_die_no_modifier(self, mock_generator):
         spec = RollSpec(dice_count=1, dice_sides=13)
-        mock_random.return_value = 3
+        mock_generator.return_value = TestGenerator([3])
         roller = StandardRoller()
         result = RollResult(spec=spec)
         roller.roll_dice_set(spec, result)
         self.assertEqual(result.total, 3)
 
-    @patch("random.randint")
-    def test_roll_dice_set_multiple_die_no_modifier(self, mock_random):
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_set_multiple_die_no_modifier(self, mock_generator):
         spec = RollSpec(dice_count=4, dice_sides=13)
-        mock_random.side_effect = [1, 2, 3, 5]
+        mock_generator.return_value = TestGenerator([1, 2, 3, 5])
         roller = StandardRoller()
         result = RollResult(spec=spec)
         roller.roll_dice_set(spec, result)
         self.assertEqual(result.total, 1 + 2 + 3 + 5)
 
-    @patch("random.randint")
-    def test_roll_dice_set_single_die_positive_modifier(self, mock_random):
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_set_single_die_positive_modifier(self, mock_generator):
         spec = RollSpec(dice_count=1, dice_sides=13, modifier=7)
-        mock_random.side_effect = [1]
+        mock_generator.return_value = TestGenerator([1])
 
         roller = StandardRoller()
 
         result = roller.roll(spec)
         self.assertEqual(1 + 7, result.total)
 
-    @patch("random.randint")
-    def test_roll_dice_set_multiple_die_positive_modifier(self, mock_random):
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_set_multiple_die_positive_modifier(self, mock_generator):
         spec = RollSpec(dice_count=3, dice_sides=13, modifier=7)
-        mock_random.side_effect = [1, 2, 3]
+        mock_generator.return_value = TestGenerator([1, 2, 3])
 
         roller = StandardRoller()
         result = roller.roll(spec)
         self.assertEqual(result.total, 6 + 7)
 
-    @patch("random.randint")
-    def test_roll_dice_set_multiple_die_negative_modifier(self, mock_random):
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_set_multiple_die_negative_modifier(self, mock_generator):
         spec = RollSpec(dice_count=3, dice_sides=13, modifier=-7)
-        mock_random.side_effect = [1, 2, 3]
+        mock_generator.return_value = TestGenerator([1, 2, 3])
 
         roller = StandardRoller()
 
         result = roller.roll(spec)
         self.assertEqual(6 - 7, result.total)
 
-    @patch("random.randint")
-    def test_roll_dice_happy(self, mock_random):
-        mock_random.return_value = 3
+    @patch("roll_witch.dice_bot.roller.generator.get_instance")
+    def test_roll_dice_happy(self, mock_generator):
+        mock_generator.return_value = TestGenerator([3])
         spec = RollSpec(dice_count=1, dice_sides=13)
         roller = StandardRoller()
         value = roller.roll_dice(spec)
