1b453b55beb195e0b05d20546d908fbdffd7ecaf
DavidWylie <david.leo.wylie@googlemail.com>
2020-10-27 12:39:27 +0000

    Add basic multiplication support

--- roll_witch/dice_bot/input/spec/modifier.py
+++ roll_witch/dice_bot/input/spec/modifier.py
@@ -8,16 +8,15 @@ from roll_witch.dice_bot.spec import RollSpec
 class ModifierSpec(InputPartSpec):
     def __init__(self) -> None:
         super().__init__()
-        self.regex = re.compile(r"([+\-])*([0-9]+)")
+        self.regex = re.compile(r"([+\-*])*([0-9]+)")
         self.name = "modifier_spec"
 
     def apply(self, match: Match):
-        if match.group(1) == "+":
+        operator = match.group(1)
+        operators = ["+", "-", "*"]
+        if operator in operators:
             modifier = int(match.group(2))
-            return RollSpec(modifier=modifier, operation="+")
-        elif match.group(1) == "-":
-            modifier = int(match.group(2))
-            return RollSpec(modifier=modifier, operation="-")
+            return RollSpec(modifier=modifier, operation=operator)
         else:
             modifier = int(match.group(2))
             return RollSpec(modifier=modifier, operation="+")
