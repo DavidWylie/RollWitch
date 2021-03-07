94505b77dd97e59e4ba0db99af58fcfda348f5cc
DavidWylie <david.leo.wylie@googlemail.com>
2021-03-07 22:28:53 +0000

    add testing for spacing.

--- tests/test_get_command.py
+++ tests/test_get_command.py
@@ -4,9 +4,11 @@ from roll_witch.rolling import command
 
 
 class TestGetCommand(TestCase):
-    @patch("random.randint")
-    def test_invalid_command(self, mock_roll):
-        mock_roll.side_effect = [1,2,3,4,5,6]
+    def test_invalid_command(self):
         response, input_text = command.get_command("hello")
-
         self.assertIsNone(response)
+
+    def test_spacing_in_commands(self):
+        response, input_text = command.get_command("! Roll 1d6")
+        expected, test_input_text = command.get_command("!roll 1d6")
+        self.assertEqual(expected, response)
