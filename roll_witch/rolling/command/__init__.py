dc5b07662284c506ae18a5a7c86b627def0b7e41
DavidWylie-ZUK <david.wylie@zuhlke.com>
2024-01-04 13:41:37 +0000

    linting

--- roll_witch/rolling/command/__init__.py
+++ roll_witch/rolling/command/__init__.py
@@ -19,6 +19,6 @@ def get_command(message_content: str):
     clean_command_string = clean_command(message_content)
     for prefix, op_getter in operations.items():
         if clean_command_string.startswith(prefix):
-            operation_input = clean_command_string[len(prefix) :].lstrip()
+            operation_input = clean_command_string[len(prefix):].lstrip()
             return op_getter, operation_input
     return None, message_content
